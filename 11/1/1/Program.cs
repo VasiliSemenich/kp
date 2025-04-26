cf2cf05019d689f712a23778e8737b01373c3fbd
Semenich Vasil <spamervasa@gmail.com>
2025-04-26 11:26:05 +0300

    11

--- 11/1/1/Program.cs
+++ 11/1/1/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using System;
+
+class Program
+{
+    // Базовый класс
+    public class BaseClass
+    {
+        public virtual string Name => "BaseClass";
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Поля: Нет дополнительных полей");
+        }
+    }
+
+    // Производный класс 1
+    public class DerivedClass1 : BaseClass
+    {
+        public int Field1 { get; set; }
+        public override string Name => "DerivedClass1";
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Поле: Field1 = {Field1}");
+        }
+    }
+
+    // Производный класс 2
+    public class DerivedClass2 : BaseClass
+    {
+        public string Field2 { get; set; }
+        public override string Name => "DerivedClass2";
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Поле: Field2 = {Field2}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Создание объектов
+        BaseClass obj1 = new BaseClass();
+        BaseClass obj2 = new DerivedClass1 { Field1 = 42 };
+        BaseClass obj3 = new DerivedClass2 { Field2 = "Hello" };
+        BaseClass obj4 = new DerivedClass1 { Field1 = 15 };
+        BaseClass obj5 = new DerivedClass2 { Field2 = "World" };
+
+        // Полиморфный контейнер - массив ссылок базового класса
+        BaseClass[] objects = { obj1, obj2, obj3, obj4, obj5 };
+
+        // Вывод результатов работы методов на экран
+        Console.WriteLine("Протокол работы:");
+        foreach (var obj in objects)
+        {
+            obj.DisplayInfo();
+        }
+    }
+}
---
+++ 11/1/2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    // Абстрактный класс
+    public abstract class BaseClass
+    {
+        public abstract string Name { get; }
+        public abstract double GetIncome();
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Доход: {GetIncome()}");
+        }
+    }
+
+    // Производный класс 1
+    public class DerivedClass1 : BaseClass
+    {
+        public double Salary { get; set; }
+
+        public override string Name => "DerivedClass1";
+
+        public override double GetIncome()
+        {
+            return Salary;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Зарплата: {Salary}, Доход: {GetIncome()}");
+        }
+    }
+
+    // Производный класс 2
+    public class DerivedClass2 : BaseClass
+    {
+        public double HourlyRate { get; set; }
+        public int HoursWorked { get; set; }
+
+        public override string Name => "DerivedClass2";
+
+        public override double GetIncome()
+        {
+            return HourlyRate * HoursWorked;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Класс: {Name}, Почасовая ставка: {HourlyRate}, Отработано часов: {HoursWorked}, Доход: {GetIncome()}");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Создание объектов
+        BaseClass obj1 = new DerivedClass1 { Salary = 50000 };
+        BaseClass obj2 = new DerivedClass2 { HourlyRate = 20, HoursWorked = 160 };
+        BaseClass obj3 = new DerivedClass1 { Salary = 75000 };
+        BaseClass obj4 = new DerivedClass2 { HourlyRate = 25, HoursWorked = 120 };
+        BaseClass obj5 = new DerivedClass2 { HourlyRate = 30, HoursWorked = 140 };
+
+        // Полиморфный контейнер - массив объектов базового класса
+        BaseClass[] objects = { obj1, obj2, obj3, obj4, obj5 };
+
+        double totalIncome = 0;
+
+        // Вывод протокола работы и расчет суммарного дохода
+        Console.WriteLine("Протокол работы:");
+        foreach (var obj in objects)
+        {
+            obj.DisplayInfo();
+            totalIncome += obj.GetIncome();
+        }
+
+        Console.WriteLine($"\nСуммарный доход: {totalIncome}");
+    }
+}
