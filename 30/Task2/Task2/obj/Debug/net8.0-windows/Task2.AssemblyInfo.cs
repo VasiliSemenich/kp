4e8ef48ff3472c11ee45cafc14b3a467aa1766ac
Semenich Vasil <spamervasa@gmail.com>
2025-05-23 22:39:34 +0300

    31

--- 30/Task2/Task2/obj/Debug/net8.0-windows/Task2.AssemblyInfo.cs
+++ 30/Task2/Task2/obj/Debug/net8.0-windows/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77131701e066046a4a10bf5f8a414ed35e57664")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1aebc648086e80626e3eea1c205b1e052f390d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
